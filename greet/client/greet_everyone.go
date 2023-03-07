486c6f0697b0400eb49c0492ddd3cf90148fa129
Gidraff <kamandegid@gmail.com>
2023-03-07 06:51:13 +0300

    chore: project setup

---
+++ greet/client/greet_everyone.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"time"
+
+	pb "github.com/Gidraff/sctp30/greet/proto"
+)
+
+func doGreetEveryone(c pb.GreetServiceClient) {
+	log.Println("doGreetEveryone was invoked")
+
+	stream, err := c.GreetEveryOne(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error while creating stream: %v\n", err)
+	}
+
+	reqs := []*pb.GreetRequest{
+		{FirstName: "Gidraff"},
+		{FirstName: "Karanja"},
+		{FirstName: "Kamande"},
+	}
+
+	waitc := make(chan struct{})
+
+	go func() {
+		for _, req := range reqs {
+			log.Printf("Send request: %v\n", req)
+			stream.Send(req)
+			time.Sleep(1 * time.Second)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				log.Printf("Error while receiving: %v\n", err)
+				break
+			}
+			log.Printf("Received: %v\n", res.Result)
+		}
+		close(waitc)
+	}()
+
+	<-waitc
+}
